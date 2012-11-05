9c3210c79fc3f6f09db07fe1f50377862d3e012e
Morten Trydal <trydis@hotmail.com>
2012-11-05 21:16:18 +0100

    Added support for getting all teams

--- Parameter/Team.cs
+++ Parameter/Team.cs
@@ -1,7 +1,20 @@
+using System.Collections.Generic;
+
 namespace UltimateTeam.Toolkit.Parameter
 {
-    public class Team
+    public class Team : SearchParameterBase<uint>
     {
         public const uint ManchesterUnited = 11;
+
+        private Team(string description, uint value)
+        {
+            Description = description;
+            Value = value;
+        }
+
+        public static IEnumerable<Team> GetAll()
+        {
+            yield return new Team("Manchester United", ManchesterUnited);
+        }
     }
 }
