7ef194c520e44e52e6b1625f3f848857a167231d
Morten Trydal <trydis@hotmail.com>
2012-11-10 00:12:17 +0100

    Mapped the well-known nations

--- Parameter/Nation.cs
+++ Parameter/Nation.cs
@@ -4,7 +4,25 @@ namespace UltimateTeam.Toolkit.Parameter
 {
     public class Nation : SearchParameterBase<uint>
     {
+        public const uint Argentina = 52;
+
+        public const uint Brazil = 54;
+
+        public const uint England = 14;
+
+        public const uint France = 18;
+
+        public const uint Germany = 21;
+
+        public const uint Italy = 27;
+
+        public const uint Netherlands = 34;
+
         public const uint Norway = 36;
+
+        public const uint Portugal = 38;
+
+        public const uint Spain = 45;
 
         private Nation(string description, uint value)
         {
@@ -14,7 +32,16 @@ namespace UltimateTeam.Toolkit.Parameter
 
         public static IEnumerable<Nation> GetAll()
         {
+            yield return new Nation("Argentina", Argentina);
+            yield return new Nation("Brazil", Brazil);
+            yield return new Nation("England", England);
+            yield return new Nation("France", France);
+            yield return new Nation("Germany", Germany);
+            yield return new Nation("Italy", Italy);
+            yield return new Nation("Netherlands", Netherlands);
             yield return new Nation("Norway", Norway);
+            yield return new Nation("Portugal", Portugal);
+            yield return new Nation("Spain", Spain);
         }
     }
 }
