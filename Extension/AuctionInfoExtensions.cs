ac9a38a573ce3697c2c130f1a9012698b6781870
Morten Trydal <trydis@hotmail.com>
2012-11-15 21:54:50 +0100

    Moved and adjusted the method for calculating bid
    It's now an extension method on the AuctionInfo class. This makes it
    easier for the clients, as they don't need to think about whether the
    auctions have any bids or not.

---
+++ Extension/AuctionInfoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using UltimateTeam.Toolkit.Model;
+
+namespace UltimateTeam.Toolkit.Extension
+{
+    public static class AuctionInfoExtensions
+    {
+        public static uint CalculateBid(this AuctionInfo auctionInfo)
+        {
+            var currentPrice = auctionInfo.CurrentPrice == 0 ? auctionInfo.StartingBid : auctionInfo.CurrentPrice;
+
+            if (currentPrice < 1000)
+                return currentPrice + 50;
+
+            if (currentPrice < 10000)
+                return currentPrice + 100;
+
+            if (currentPrice < 50000)
+                return currentPrice + 250;
+
+            if (currentPrice < 100000)
+                return currentPrice + 500;
+
+            return currentPrice + 1000;
+        }
+    }
+}
